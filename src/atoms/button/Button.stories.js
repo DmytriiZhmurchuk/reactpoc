32b0ab1cb4ced7d095789b2a945a71c01cb35056
Dmytrii Zhmurchuk <Dmytrii_Zhmurchuk@epam.com>
2020-01-17 14:16:13 +0200

    story book add info

--- src/atoms/button/Button.stories.js
+++ src/atoms/button/Button.stories.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
 import Button from './styled-button';
 
 export default {
   component: Button,
   title: 'Styled-Button',
+  decorators: [withInfo],
+  parameters: {
+    info: {
+    	text:"description or documentation about my component, supports markdown"
+    },
+  }
 };
 
 export const usual = () => <Button onClick={action('clicked')}>Button Text</Button>;
